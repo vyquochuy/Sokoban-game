3111210740563da7a38d60c8dd30460befb27c8a
Vy Quoc Huy <huy2004abc@gmail.com>
2024-10-24 20:28:49 +0700

    test

--- GUI.py
+++ GUI.py
@@ -1,33 +1,184 @@
-import tkinter as tk
+import sys
 
-def say_hello():
-    label.config(text="Hello, World!")
+class Game:
+    def is_valid_value(self, char):
+        return char in {' ', '#', '$', '@', '.', '*', '+'}
 
-def home():
-    label.config(text="Welcome to my GUI")
-    lb = tk.Listbox(root)
-    lb.insert(1, "BFS")
-    lb.insert(2, "DFS")
-    lb.insert(3, "UCS")
-    lb.insert(4, "Astar")
-    lb.place(x=100, y=100)
-    lb.pack()
+    def __init__(self,filename,level):
+        self.queue = queue.LifoQueue()
+        self.matrix = []
+        
+        if level < 1:
+            print ("ERROR: Level "+str(level)+" is out of range")
+            sys.exit(1)
 
-# Create the main window
-root = tk.Tk()
-root.title("My First GUI")
+        with open(filename,'r') as file:
+            level_found = False
+            for line in file:
+                row = []
+                if not level_found:
+                    if  "Level "+str(level) == line.strip():
+                        level_found = True
+                else:
+                    if line.strip() != "":
+                        row = []
+                        for c in line:
+                            if c != '\n' and self.is_valid_value(c):
+                                row.append(c)
+                            elif c == '\n': #jump to next row when newline
+                                continue
+                            else:
+                                print ("ERROR: Level "+str(level)+" has invalid value "+c)
+                                sys.exit(1)
+                        self.matrix.append(row)
+                    else:
+                        break
+                    
+    def load_size(self):
+        x = 0
+        y = len(self.matrix)
+        for row in self.matrix:
+            if len(row) > x:
+                x = len(row)
+        return (x * 32+ 350, y * 32)
 
-# Create a label widget
-label = tk.Label(root, text="Welcome to my GUI", font=("Helvetica", 16))
-label.pack(pady=20)
+    def get_matrix(self):
+        return self.matrix
 
-# Create a button widget
-button = tk.Button(root, text="Start", command=home, font=("Helvetica", 14))
-button.pack(pady=10)
+    def print_matrix(self):
+        for row in self.matrix:
+            for char in row:
+                sys.stdout.write(char)
+                sys.stdout.flush()
+            sys.stdout.write('\n')
 
-# button end
-button_end = tk.Button(root, text="End", command=root.quit, font=("Helvetica", 14))
-button_end.pack(pady=10)
+    def get_content(self,x,y):
+        return self.matrix[y][x]
 
-# Start the main event loop
-root.mainloop()
+    def set_content(self,x,y,content):
+        if self.is_valid_value(content):
+            self.matrix[y][x] = content
+        else:
+            print ("ERROR: Value '"+content+"' to be added is not valid")
+
+    def Ares(self):
+        x = 0
+        y = 0
+        for row in self.matrix:
+            for pos in row:
+                if pos == '@' or pos == '+':
+                    return (x, y, pos)
+                else:
+                    x = x + 1
+            y = y + 1
+            x = 0
+
+    def can_move(self,x,y):
+        return self.get_content(self.Ares()[0]+x,self.Ares()[1]+y) not in ['#','*','$']
+
+    def next(self,x,y):
+        return self.get_content(self.Ares()[0]+x,self.Ares()[1]+y)
+
+    def can_push(self,x,y):
+        return (self.next(x,y) in ['*','$'] and self.next(x+x,y+y) in [' ','.'])
+
+    def is_completed(self):
+        for row in self.matrix:
+            for cell in row:
+                if cell == '$':
+                    return False
+        return True
+
+    def move_box(self,x,y,a,b):
+#        (x,y) -> move to do
+#        (a,b) -> box to move
+        current_box = self.get_content(x,y)
+        future_box = self.get_content(x+a,y+b)
+        if current_box == '$' and future_box == ' ':
+            self.set_content(x+a,y+b,'$')
+            self.set_content(x,y,' ')
+        elif current_box == '$' and future_box == '.':
+            self.set_content(x+a,y+b,'*')
+            self.set_content(x,y,' ')
+        elif current_box == '*' and future_box == ' ':
+            self.set_content(x+a,y+b,'$')
+            self.set_content(x,y,'.')
+        elif current_box == '*' and future_box == '.':
+            self.set_content(x+a,y+b,'*')
+            self.set_content(x,y,'.')
+
+    def unmove(self):
+        if not self.queue.empty():
+            movement = self.queue.get()
+            if movement[2]:
+                current = self.Ares()
+                self.move(movement[0] * -1,movement[1] * -1, False)
+                self.move_box(current[0]+movement[0],current[1]+movement[1],movement[0] * -1,movement[1] * -1)
+            else:
+                self.move(movement[0] * -1,movement[1] * -1, False)
+
+    def move(self,x,y,save):
+        if self.can_move(x,y):
+            current = self.Ares()
+            future = self.next(x,y)
+            if current[2] == '@' and future == ' ':
+                self.set_content(current[0]+x,current[1]+y,'@')
+                self.set_content(current[0],current[1],' ')
+                if save: self.queue.put((x,y,False))
+            elif current[2] == '@' and future == '.':
+                self.set_content(current[0]+x,current[1]+y,'+')
+                self.set_content(current[0],current[1],' ')
+                if save: self.queue.put((x,y,False))
+            elif current[2] == '+' and future == ' ':
+                self.set_content(current[0]+x,current[1]+y,'@')
+                self.set_content(current[0],current[1],'.')
+                if save: self.queue.put((x,y,False))
+            elif current[2] == '+' and future == '.':
+                self.set_content(current[0]+x,current[1]+y,'+')
+                self.set_content(current[0],current[1],'.')
+                if save: self.queue.put((x,y,False))
+                
+        elif self.can_push(x,y):
+            current = self.Ares()
+            future = self.next(x,y)
+            future_box = self.next(x+x,y+y)
+            if current[2] == '@' and future == '$' and future_box == ' ':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],' ')
+                self.set_content(current[0]+x,current[1]+y,'@')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '@' and future == '$' and future_box == '.':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],' ')
+                self.set_content(current[0]+x,current[1]+y,'@')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '@' and future == '*' and future_box == ' ':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],' ')
+                self.set_content(current[0]+x,current[1]+y,'+')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '@' and future == '*' and future_box == '.':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],' ')
+                self.set_content(current[0]+x,current[1]+y,'+')
+                if save: self.queue.put((x,y,True))
+            if current[2] == '+' and future == '$' and future_box == ' ':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],'.')
+                self.set_content(current[0]+x,current[1]+y,'@')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '+' and future == '$' and future_box == '.':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],'.')
+                self.set_content(current[0]+x,current[1]+y,'+')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '+' and future == '*' and future_box == ' ':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],'.')
+                self.set_content(current[0]+x,current[1]+y,'+')
+                if save: self.queue.put((x,y,True))
+            elif current[2] == '+' and future == '*' and future_box == '.':
+                self.move_box(current[0]+x,current[1]+y,x,y)
+                self.set_content(current[0],current[1],'.')
+                self.set_content(current[0]+x,current[1]+y,'+')
+                if save: self.queue.put((x,y,True))
