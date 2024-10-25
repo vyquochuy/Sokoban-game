859aefb02fafb5332fb89cd49f98f71b234837bc
Vy Quoc Huy <huy2004abc@gmail.com>
2024-10-25 18:09:48 +0700

    change background

--- Source/Map.py
+++ Source/Map.py
@@ -4,15 +4,14 @@ import queue
 
 pygame.init()
 
-# Kích thước ô vuông
-tile_size = 50
+# Kích thước MÀN HÌNH
+SCREEN_WIDTH = 1440
+SCREEN_HEIGHT = 900
 
 # tiêu đề
 pygame.display.set_caption("Ares's Adventure")
 
 class Game:
-    cost = 0
-
     def is_valid_value(self, char):
         return char in [' ', '#', '@', '.', '+', '$', '*']
 
@@ -50,20 +49,18 @@ class Game:
                     break
 
         # Tính kích thước màn hình dựa trên ma trận
-        self.screen_width = max(len(row) for row in self.matrix) * tile_size
-        self.screen_height = len(self.matrix) * tile_size
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.tile_size = SCREEN_HEIGHT  // len(self.matrix)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
         # Tải hình ảnh và thay đổi kích thước
-        self.wall = pygame.transform.scale(pygame.image.load("img/wall.png"), (tile_size, tile_size))
-        self.floor = pygame.transform.scale(pygame.image.load('img/floor.png'), (tile_size, tile_size))
-        self.stone = pygame.transform.scale(pygame.image.load('img/stone.png'), (tile_size, tile_size))
-        self.stone_on_stock = pygame.transform.scale(pygame.image.load('img/stone on stock.png'), (tile_size, tile_size))
-        self.Ares = pygame.transform.scale(pygame.image.load('img/Ares.png').convert_alpha(), (tile_size, tile_size))
-        self.Ares_on_stock = pygame.transform.scale(pygame.image.load('img/Ares on stock.png'), (tile_size, tile_size))
-        self.stock = pygame.transform.scale(pygame.image.load('img/stock.png'), (tile_size, tile_size))
-
-    def in_of_wall(self, x, y):
+        self.wall = pygame.transform.scale(pygame.image.load("img/wall.png"), (self.tile_size, self.tile_size))
+        self.floor = pygame.transform.scale(pygame.image.load('img/floor.png'), (self.tile_size, self.tile_size))
+        self.stone = pygame.transform.scale(pygame.image.load('img/stone.png'), (self.tile_size, self.tile_size))
+        self.stone_on_stock = pygame.transform.scale(pygame.image.load('img/stone on stock.png'), (self.tile_size, self.tile_size))
+        self.Ares = pygame.transform.scale(pygame.image.load('img/Ares.png').convert_alpha(), (self.tile_size, self.tile_size))
+        self.Ares_on_stock = pygame.transform.scale(pygame.image.load('img/Ares on stock.png'), (self.tile_size, self.tile_size))
+        self.stock = pygame.transform.scale(pygame.image.load('img/stock.png'), (self.tile_size, self.tile_size))
+    def in_wall(self, x, y):
         # Kiểm tra có bao quanh bởi ít nhất 4 bức tường hay không
         if x == 0 or y == 0 or x == len(self.matrix) - 1 or y == len(self.matrix[x]) - 1:
             return False
@@ -100,26 +97,28 @@ class Game:
                 continue
         return wall_count >= 4
     
-    def draw_map(self):                    
+    def draw_map(self):         
+        # VẼ BACKGROUND
+        self.screen.fill((154, 126, 111))
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[i])):
                 # Vẽ nền
-                if self.in_of_wall(i, j):
-                    self.screen.blit(self.floor, (j * tile_size, i * tile_size))
+                if self.in_wall(i, j):
+                    self.screen.blit(self.floor, (j * self.tile_size, i * self.tile_size))
                 
                 # Vẽ các đối tượng dựa trên ký tự trong ma trận
                 if self.matrix[i][j] == '#':
-                    self.screen.blit(self.wall, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.wall, (j * self.tile_size, i * self.tile_size))
                 elif self.matrix[i][j] == '@':
-                    self.screen.blit(self.Ares, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.Ares, (j * self.tile_size, i * self.tile_size))
                 elif self.matrix[i][j] == '.':
-                    self.screen.blit(self.stock, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.stock, (j * self.tile_size, i * self.tile_size))
                 elif self.matrix[i][j] == '+':
-                    self.screen.blit(self.Ares_on_stock, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.Ares_on_stock, (j * self.tile_size, i * self.tile_size))
                 elif self.matrix[i][j] == '$':
-                    self.screen.blit(self.stone, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.stone, (j * self.tile_size, i * self.tile_size))
                 elif self.matrix[i][j] == '*':
-                    self.screen.blit(self.stone_on_stock, (j * tile_size, i * tile_size))
+                    self.screen.blit(self.stone_on_stock, (j * self.tile_size, i * self.tile_size))
 
         pygame.display.flip()
 
